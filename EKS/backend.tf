dfe8c1941d5e20c0c2684fa3063aaf28c7e4e898
srprithviraj <147120247+srprithviraj@users.noreply.github.com>
2024-01-11 22:19:51 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "infra-terraform-bucket-v1"  # Replace with your actual S3 bucket name
+    bucket = "s3-bucket-terraform12"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "ap-south-1" 
+    region = "us-west-2" 
   }
 }
